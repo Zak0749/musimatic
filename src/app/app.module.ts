05556f69ff091e59926026b2c945bcdc16f8159d
Zak <89150707+Zak0749@users.noreply.github.com>
2021-11-26 17:02:17 +0000

    cube go brr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SceneComponent } from './scene/scene.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SceneComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
