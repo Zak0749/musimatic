aa59c87a5596de8d1cc81839fe4e48f4bd521f6e
Zak <89150707+Zak0749@users.noreply.github.com>
2021-11-27 13:30:39 +0000

    build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'threejs-agular';
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    (<any>window).twttr.widgets.load();
+  }
 }
--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import {
   AfterViewInit,
   Component,
@@ -5,15 +6,29 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
+import { gsap } from 'gsap';
+import { Bounce, ScrollTrigger } from 'gsap/all';
+import { fromEvent } from 'rxjs';
 import {
+  AmbientLight,
   BoxGeometry,
+  Camera,
+  Color,
+  GridHelper,
   Mesh,
   MeshBasicMaterial,
+  MeshStandardMaterial,
   PerspectiveCamera,
+  PointLight,
   Scene,
+  SphereGeometry,
+  Texture,
+  TorusGeometry,
   WebGLRenderer,
 } from 'three';
+import * as THREE from 'three';
 import { WebGL1Renderer } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'app-scene',
@@ -23,56 +38,84 @@ import { WebGL1Renderer } from 'three';
 export class SceneComponent implements OnInit, AfterViewInit {
   @ViewChild('canvas')
   canvasRef?: ElementRef;
+  scene?: Scene;
+  camera?: PerspectiveCamera;
+  renderer?: WebGLRenderer;
+  geometry?: BoxGeometry;
+  material?: MeshStandardMaterial;
+  object?: Mesh;
+  pointLight?: PointLight;
+  ambientLight?: AmbientLight;
 
-  camera!: PerspectiveCamera;
-
-  get canvas(): HTMLCanvasElement {
-    return this.canvasRef?.nativeElement;
-  }
-  geometry = new BoxGeometry(1, 1, 1);
-  material = new MeshBasicMaterial({
-    color: 0xff6347,
-  });
-
-  cube: THREE.Mesh = new Mesh(this.geometry, this.material);
-
-  renderer!: THREE.WebGLRenderer;
-
-  scene!: THREE.Scene;
-
-  animateCube() {
-    this.cube.rotation.x += 1;
-    this.cube.rotation.y += 1;
+  get height() {
+    return Math.max(
+      document.body.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.clientHeight,
+      document.documentElement.scrollHeight,
+      document.documentElement.offsetHeight
+    );
   }
 
-  createScene(canvas: any) {
+  get currentScroll() {
+    return document.body.getBoundingClientRect().top;
+  }
+
+  get persentDownPage() {
+    return (this.currentScroll / this.height) * 100;
+  }
+
+  get position() {
+    return Math.asin(this.persentDownPage);
+  }
+  createScene(): void {
     this.scene = new Scene();
-    this.scene.add(this.cube);
-    let aspectRatio = canvas.clientWidth / canvas.clientHeight;
-    this.camera = new PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+    this.camera = new PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight
+    );
+    this.renderer = new WebGLRenderer({
+      canvas: this.canvasRef?.nativeElement,
+    });
+
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.camera.position.setZ(30);
+
+    this.geometry = new BoxGeometry(15, 30, 2.5);
+    this.material = new MeshStandardMaterial({ color: 0xff6347 });
+    this.object = new Mesh(this.geometry, this.material);
+    this.object.position.setX(window.innerWidth < 500 ? 0 : 20);
+
+    this.pointLight = new PointLight(0xffffff);
+    this.pointLight.position.set(20, 20, 20);
+
+    this.ambientLight = new AmbientLight(0xffffff);
+
+    this.scene.add(this.pointLight, this.ambientLight, this.object);
+
+    this.scene.background = new Color(0xffffff);
+
+    this.renderer.render(this.scene, this.camera);
+
+    const { renderer, object, camera } = this;
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      renderer.render(this.scene!, this.camera!);
+    };
+
+    fromEvent(window, 'scroll').subscribe((e) => {
+      object.rotation.y = this.persentDownPage / 10;
+    });
+
+    animate();
   }
-
-  startRenderingLoop(canvas: any) {
-    this.renderer = new WebGLRenderer({ canvas });
-    this.renderer.setPixelRatio(devicePixelRatio);
-    this.renderer.setSize(canvas.clientWidth, canvas.clientHeight);
-
-    let component = this;
-    (function render() {
-      requestAnimationFrame(render);
-      component.animateCube();
-      component.renderer.render(component.scene, component.camera);
-    })();
-  }
-
-  constructor() {}
 
   ngOnInit(): void {}
 
-  ngAfterViewInit() {
-    console.log(this.canvasRef);
-    this.createScene(this.canvas);
-    this.startRenderingLoop(this.canvas);
+  ngAfterViewInit(): void {
+    this.createScene();
   }
 }
